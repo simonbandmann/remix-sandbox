6a660e1f527c23ee34e9cfdccf73d190efd50993
simonbandmann <s.bandmann@gmail.com>
2023-10-16 09:02:24 +0200

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <p id='index-page'>
+      This is a demo for Remix. I was here.
+      <br />
+      Check out <a href='https://remix.run'>the docs at remix.run</a>.
+    </p>
+  );
+}
